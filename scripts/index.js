5a3993ffa1775f26c3480a85bea950e0bace22a6
HIMU005 <hashanuzzaman99@gamil.com>
2024-02-16 14:35:25 +0600

    add score and life

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+function cathThePress(event) {
+    const playerPressed = event.key;
+    
+    const currentExpectElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentExpectElement.innerText.toLowerCase();
+
+    console.log(playerPressed, currentAlphabet);
+    if(playerPressed === currentAlphabet) {
+        console.log('You win');
+        removeBackgroundColor(playerPressed);
+
+        const currentScore= getTextValueById('current-score');
+        const newScore = currentScore+ 1;
+        setTextValueById('current-score', newScore)
+        console.log('currentScore', newScore);
+
+        startTheGame();
+    }
+    else {
+        console.log('try again');
+        const currentLife =getTextValueById('current-life')
+        const newLife = currentLife-1;
+        setTextValueById('current-life', newLife);
+    }
+}
+
+document.addEventListener('keyup',cathThePress);
+
 function startTheGame() {
     // generate random number 
     const alphabet = getRandomAlphabet();
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,13 +1,16 @@
+// hide element 
 function hideElementById(elementId)
 {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
+// show element 
 function showElementByID(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
 
+// get random number 
 function getRandomAlphabet() {
     const alphabetsString = "abcdefghijklmnopqrstuvwxyz";
     const alphabets = alphabetsString.split('');
@@ -18,12 +21,24 @@ function getRandomAlphabet() {
     const alphabet = alphabets[randomNum];
     return alphabet;
 }
+// set background 
 function setBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-500');
 }
-
+// remove background 
 function removeBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-500');
+}
+
+function getTextValueById(elementID) {
+    const element = document.getElementById(elementID);
+    const value = parseInt(element.innerText);
+    return value;
+}
+
+function setTextValueById(elementID, value) {
+    const element = document.getElementById(elementID);
+    element.innerText =value;
 }
